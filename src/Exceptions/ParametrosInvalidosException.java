dcefb2220d0e9413a6ff5a80d962636238f5c578
Francisco Junior <francisco.junior@theos.com.br>
2024-05-17 09:38:33 -0300

    desafio controle de fluxo

--- src/Exceptions/ParametrosInvalidosException.java
+++ src/Exceptions/ParametrosInvalidosException.java
@@ -1,5 +1,7 @@
 package Exceptions;
 
 public class ParametrosInvalidosException extends Exception {
-    
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
 }
